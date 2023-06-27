a7feec99bb5ea654b05dfbb377ed1ba4663aa556
bipblop555 <tom.bonnet@hotmail.com>
2023-06-27 19:09:27 +0200

    dump.sql au montage container

---
+++ db/dump.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE db;
+
+\c db;
+
+CREATE TABLE users (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL
+);
