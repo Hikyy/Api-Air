e3ee6fd3f80ab5e09591547a659586f09b1624ec
bipblop555 <tom.bonnet@hotmail.com>
2023-06-27 19:26:59 +0200

    suppression fct login obsolete login

--- internal/handlers/userHandler.go
+++ internal/handlers/userHandler.go
@@ -10,32 +10,6 @@ import (
 	"fmt"
 )
 
-func Login(writer http.ResponseWriter, request *http.Request) {
-	var user requests.StoreUserRequest
-	body, err := io.ReadAll(request.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// if err = json.Unmarshal(body, &user); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	fmt.Printf("It's %s\n", body)
-
-	value := reflect.ValueOf(user)
-	printFieldKeysAndValues(value)
-}
-
-func Register(w http.ResponseWriter, r *http.Request){
-	r.Header().Set("Content-Type", "application/json")
-	
-	var user User json.NewDecoder(r.Body).Decode(&user)
-	user.Password = getHash([]byte(user.Password))
-}
-
-
 func printFieldKeysAndValues(value reflect.Value) {
 	request := value.Type()
 	for i := 0; i < value.NumField(); i++ {
