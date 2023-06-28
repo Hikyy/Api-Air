6433876423783e419a94864d7aa4ca9812e58dff
bipblop555 <tom.bonnet@hotmail.com>
2023-06-28 07:42:04 +0200

    modif dump varchar(255)

--- db/dump.sql
+++ db/dump.sql
@@ -5,6 +5,6 @@ CREATE DATABASE db;
 CREATE TABLE users (
 	user_id serial PRIMARY KEY,
 	name VARCHAR ( 50 ) UNIQUE NOT NULL,
-	email VARCHAR ( 255 ) UNIQUE NOT NULL,
-	password VARCHAR ( 50 ) NOT NULL
+	email VARCHAR ( 100 ) UNIQUE NOT NULL,
+	password VARCHAR ( 255 ) NOT NULL
 );
