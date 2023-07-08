bcfe9d76f959e59b2caf881a75d1bc6ac0500b34
bipblop555 <tom.bonnet@hotmail.com>
2023-07-08 22:33:55 +0200

    Revert "m-a-j main.go"
    This reverts commit 72bfc4deba48284721315d1dc4c61921ac94344b.

--- app/main.go
+++ app/main.go
@@ -55,7 +55,7 @@ func main() {
 	r.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Println("w => ", &w)
-		usersC.Create(w, r, "Users")
+		usersC.Create(w, r)
 	}).Methods("POST")
 
 	r.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
