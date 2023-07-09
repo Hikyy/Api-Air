353f6519b73040af18f152f203482ea3512d592c
Rayane <mabroukr1997@gmail.com>
2023-07-09 22:10:53 +0200

    Update User Struct

---
+++ internal/handlers/types.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"App/internal/models"
+)
+
+// Users Struct for holding Users variables
+type Users struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+	us       models.EntityImplementService
+}
