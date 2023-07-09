60091ada2755288c46b0c00ae13e3f7cc3040765
bipblop555 <tom.bonnet@hotmail.com>
2023-07-10 01:18:35 +0200

    modif requests

--- internal/requests/userRegisterRequest.go
+++ internal/requests/userRegisterRequest.go
@@ -4,10 +4,11 @@ type StoreUserRequest struct {
 	Data struct {
 		Type       string `json:"type"`
 		Attributes struct {
-			Email     string `json:"email_address" validate:"email|min=6|required"`
-			Firstname string `json:"firstname" validate:"max=255|required"`
-			Lastname  string `json:"lastname" validate:"max=255|required"`
-			Password  string `json:"password" validate:"max=255|required"`
+			Email      string `json:"email_address" validate:"email|min=6|required"`
+			Firstname  string `json:"firstname" validate:"max=255|required"`
+			Lastname   string `json:"lastname" validate:"max=255|required"`
+			Password   string `json:"password" validate:"max=255|required"`
+			Group_name string `json:"default:'admin'"`
 		} `json:"attributes"`
 	} `json:"data"`
 }
