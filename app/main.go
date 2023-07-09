1a761c2724f1196ce4d13ed8960c3f27fb3c111d
Rayane <mabroukr1997@gmail.com>
2023-07-09 22:05:57 +0200

    improves code readability

--- app/main.go
+++ app/main.go
@@ -1,72 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
-	"net/http"
-
-	"App/internal/controllers"
+	"App/internal/handlers"
 	"App/internal/models"
+	"fmt"
+	"net/http"
 )
 
-var psqlInfo string
 var broker = "mqtt://mqtt.arcplex.fr:2295" // Remplacez par l'adresse et le port de votre broker MQTT
 var clientID = "go-mqtt-client"
 
 func main() {
-
-	var (
-		host     = "127.0.0.1"
-		port     = "3307"
-		dbuser   = "root"
-		password = "root"
-		dbname   = "postgres"
-	)
-
-	// test SetMQTT
-	//handlers.SetMQTT(broker, clientID)
-
-	psqlInfo = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, dbuser, password, dbname)
-
-	us, err := models.NewUserService(psqlInfo)
-	fmt.Println(us, err)
-
-	listen := "localhost:8097"
+	err := models.DatabaseServiceProvider()
 
 	if err != nil {
-		fmt.Println(err)
-		panic(err)
+		fmt.Println("Failed to connect to database:", err)
+		return
+	}
+	defer models.InitGorm.Close()
+
+	if err != nil {
+		fmt.Println("Failed to connect to database:", err)
+		return
 	}
 
-	us.Ping()
+	router := handlers.SetupRouter()
 
-	usersC := controllers.NewUsers(us) // Handling User Controller
-	r := mux.NewRouter()
+	fmt.Println("Server listening on port 8097")
 
-	r.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Println("w => ", &w)
-		usersC.Create(w, r)
-	}).Methods("POST")
+	// err = godotenv.Load()
 
-	r.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		usersC.Login(w, r)
-	}).Methods("POST")
-
-	r.HandleFunc("/profil", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		fmt.Println("Profile")
-		usersC.GetAllUsers(w, r)
-	}).Methods("GET")
-
-	r.HandleFunc("/profil/update/{id}", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		usersC.UpdateRoles(w, r, "admin")
-	}).Methods("PATCH")
-
-	fmt.Println("listening on port ", listen)
-
-	http.ListenAndServe(listen, r)
+	http.ListenAndServe(":8097", router)
 }
