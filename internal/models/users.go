683c7f3f5716fb045ab4cd1e8d854e9d6dadc64c
Rayane <mabroukr1997@gmail.com>
2023-07-09 22:09:35 +0200

    Modifies function implementations in the "DbGorm" and "DatabaseProvider"

--- internal/models/users.go
+++ internal/models/users.go
@@ -1,63 +1,34 @@
 package models
 
 import (
-	"App/internal/modules/hash"
 	"encoding/json"
-	"errors"
+	// "errors"
 	"fmt"
-	"github.com/jinzhu/gorm"
-	"golang.org/x/crypto/bcrypt"
+	"reflect"
+	"strconv"
 
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
-func NewUserService(connectionInfo string) (UserService, error) {
-	ug, err := newUserGorm(connectionInfo)
-	if err != nil {
-		return nil, err
-	}
-	hmac := hash.NewHMAC(HmacSecret)
-	uv := &userValidator{
-		UserDB: ug,
-		hmac:   hmac,
-	}
-	return &userService{
-		UserDB: uv,
-	}, nil
-}
-
 // ByEmail pour get user by email
-func (ug *userGorm) ByEmail(email string) (*User, error) {
+func (ug *DbGorm) ByEmail(email string) (*User, error) {
 	var user User
-	db := ug.db.Table("Users").Where("user_email = ?", email).First(&user)
+	db := ug.db.Table("users").Where("email = ?", email).First(&user)
+
 	err := first(db, &user)
 	return &user, err
 }
 
-// Implementer et retourner le userGorm
-// permet co db
-func newUserGorm(connectionInfo string) (*userGorm, error) {
-	db, err := gorm.Open("postgres", connectionInfo)
-	if err != nil {
-		return nil, err
-	}
-	db.LogMode(true)
-	return &userGorm{
-		db: db,
-	}, nil
-}
-
 // Methode Create pour add user to db
-func (ug *userGorm) Create(user *User) error {
-	fmt.Println("Composant Create !!!!")
-	// on peut choisir le nom de la table !!!!!!!!!
-	return ug.db.Table("Users").Create(user).Error
+func (ug *DbGorm) Create(entity interface{}) error {
+	return ug.db.Create(entity).Error
 }
 
 // will query the gorm.DB and get the first item from db and place it into
 // dst , if nothing is found , it will return error.
-func first(db *gorm.DB, dst interface{}) error {
-	err := db.Table("Users").First(dst).Error
+func first(db *gorm.DB, entity interface{}) error {
+	err := db.First(entity).Error
 	if err == gorm.ErrRecordNotFound {
 		return ErrNotFound
 	}
@@ -65,20 +36,28 @@ func first(db *gorm.DB, dst interface{}) error {
 }
 
 // ByID method to get a user by ID
-func (ug *userGorm) ByID(id uint) (*User, error) {
-	var user User
-	db := ug.db.Where("id = ?", id).First(&user)
-	err := first(db, &user)
-	return &user, err
+func (ug *DbGorm) ByID(id uint, entity interface{}) error {
+	db := ug.db.Where("id = ?", id).First(&entity)
+	err := first(db, &entity)
+	return err
 }
 
 // Update method to update a user in database
-func (ug *userGorm) Update(user *User, role string) error {
-	return ug.db.Model(&user).Table("Users").Where("user_firstname = ?", &user.User_firstname).Update("group_name", role).Error
+func (ug *DbGorm) Update(entity interface{}, attribute string, value string) error {
+	entityValue := reflect.Indirect(reflect.ValueOf(entity)) // Dereference the pointer if entity is a pointer
+
+	idField := entityValue.FieldByName("Id")
+
+	if !idField.IsValid() || idField.Kind() != reflect.Int || idField.Int() == 0 {
+		return ErrInvalidID
+	}
+	id := strconv.Itoa(idField.Interface().(int))
+
+	return ug.db.Model(&entity).Where("id = ?", id).Update(attribute, value).Error
 }
 
 // Authenticate Method is used for Authenticate and Validate login
-func (us *userService) Authenticate(email, password string) (*User, error) {
+func (us *DatabaseProvider) Authenticate(email, password string) (*User, error) {
 	// Vlidate if the user is existed in the database or no
 	foundUser, err := us.ByEmail(email)
 	fmt.Println("foundUser => ", foundUser)
@@ -86,7 +65,7 @@ func (us *userService) Authenticate(email, password string) (*User, error) {
 		return nil, err
 	}
 	// Compare the login based in the Hash value
-	err = bcrypt.CompareHashAndPassword([]byte(foundUser.User_password), []byte(password))
+	err = bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(password))
 
 	if err != nil {
 		switch err {
@@ -100,7 +79,8 @@ func (us *userService) Authenticate(email, password string) (*User, error) {
 	}
 	return foundUser, nil
 }
-func (ug *userGorm) GetAllUsers() ([]byte, error) {
+
+func (ug *DbGorm) GetAllUsers() ([]byte, error) {
 	var users []User
 	db := ug.db.Table("Users").Order("user_firstname").Find(&users)
 	if db.Error != nil {
@@ -108,22 +88,9 @@ func (ug *userGorm) GetAllUsers() ([]byte, error) {
 	}
 	fmt.Println(users)
 	jsonData, err := json.Marshal(users)
+
 	if err != nil {
 		return nil, err
 	}
 	return jsonData, nil
-}
-
-// CloseDB to be used as `defer us.db.Close()`
-func (ug *userGorm) Close() error {
-	return ug.db.Close()
-}
-
-func (ug *userGorm) Ping() error {
-	if err := ug.db.DB().Ping(); err != nil {
-		ug.db.DB().Close()
-		return errors.New("Connection to DB is not available")
-	}
-	fmt.Println("Connection ok (ping)", ug)
-	return nil
 }
