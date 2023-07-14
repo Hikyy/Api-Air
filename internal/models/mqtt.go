3d0996d43718ed7245824283965f8202a3eb2048
bipblop555 <tom.bonnet@hotmail.com>
2023-07-14 15:42:05 +0200

    get all topics

--- internal/models/mqtt.go
+++ internal/models/mqtt.go
@@ -1,8 +1,1 @@
 package models
-
-type SensorData struct {
-	SensorAddress string                 `json:"source_address"`
-	SensorID      int                    `json:"sensor_id"`
-	TimeEpoch     uint                   `json:"tx_time_ms_epoch"`
-	Data          map[string]interface{} `json:"data"`
-}
