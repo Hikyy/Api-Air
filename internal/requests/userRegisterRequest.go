35b9dfa69f5664dcd53ebc320b4bdfe7961601ab
Rayane <mabroukr1997@gmail.com>
2023-07-17 19:09:14 +0200

    fix rule

--- internal/requests/userRegisterRequest.go
+++ internal/requests/userRegisterRequest.go
@@ -5,9 +5,9 @@ type StoreUserRequest struct {
 		Type       string `json:"type"`
 		Attributes struct {
 			Email      string `json:"email_address" validate:"email|min=6|required"`
-			Firstname  string `json:"firstname" validate:"max=255|required"`
-			Lastname   string `json:"lastname" validate:"max=255|required"`
-			Password   string `json:"password" validate:"max=255|required"`
+			Firstname  string `json:"firstname" validate:"max=255|min=6|required"`
+			Lastname   string `json:"lastname" validate:"max=255|min=6|required"`
+			Password   string `json:"password" validate:"max=255|min=6|required"`
 			Group_name string `json:"default:'user'"`
 		} `json:"attributes"`
 	} `json:"data"`
