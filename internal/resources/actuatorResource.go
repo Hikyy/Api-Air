19b1c8327ec201d15c067e7229a2ad39db1b88e7
Rayane <mabroukr1997@gmail.com>
2023-07-17 16:19:55 +0200

    implemnt form request

---
+++ internal/resources/actuatorResource.go
@@ -0,0 +1,15 @@
+package resources
+
+
+type ActuatorResource struct {
+	Data struct {
+		Type       string `json:"type"`
+		Id              int    `json:"id"`
+		Attributes struct {
+			ActuatorName    string `json:"actuator_name"`
+			ActuatorCommand string `json:"actuator_command"`
+			DataKey         string `json:"data_key"`
+			RoomId          int    `json:"room_id"`
+		} `json:"attributes"`
+	} `json:"data"`
+}
