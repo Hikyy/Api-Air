ab445ac58bc2e52004d711fa0568ea9ad440bb54
Rayane <mabroukr1997@gmail.com>
2023-07-18 11:22:41 +0200

    update min rule

--- internal/requests/userLoginRequest.go
+++ internal/requests/userLoginRequest.go
@@ -6,7 +6,7 @@ type UserLoginRequest struct {
 		Type       string `json:"type"`
 		Attributes struct {
 			Email    string `json:"email_address" validate:"email|min=6|required"`
-			Password string `json:"password" validate:"min=6|max=255|required"`
+			Password string `json:"password" validate:"min=5|max=255|required"`
 		} `json:"attributes"`
 	} `json:"data"`
 }
--- internal/requests/userRegisterRequest.go
+++ internal/requests/userRegisterRequest.go
@@ -8,7 +8,7 @@ type StoreUserRequest struct {
 			Email      string `json:"email_address" validate:"email|min=6|required"`
 			Firstname  string `json:"firstname" validate:"max=255|required"`
 			Lastname   string `json:"lastname" validate:"max=255|required"`
-			Password   string `json:"password" validate:"max=255|min=6|required"`
+			Password   string `json:"password" validate:"max=255|min=5|required"`
 			Group_name string `json:"default:'user'`
 		} `json:"attributes"`
 	} `json:"data"`
