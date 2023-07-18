13a3e6ce056caff24f5bd081128f7b903070b635
Rayane <mabroukr1997@gmail.com>
2023-07-18 18:47:33 +0200

    implement decode id

--- internal/handlers/sensorEventHandler.go
+++ internal/handlers/sensorEventHandler.go
@@ -11,13 +11,13 @@ import (
 )
 
 func (handler *HandlerService) IndexRoomSensorEvents(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
+	id := helpers.DecodeId(chi.URLParam(r, "id"))
 
 	roomInt, err := helpers.TransformStringToInt(id)
-
 	if err != nil {
 		return
 	}
+
 	data, err := handler.use.GetAllDatasByRoom(roomInt)
 
 	if err != nil {
@@ -25,13 +25,14 @@ func (handler *HandlerService) IndexRoomSensorEvents(w http.ResponseWriter, r *h
 		return
 	}
 
-	var sensorEventResource []resources.SensorEventResource
+	var sensorEventResource []resources.SensorDataFromDate
 
 	resources.GenerateResource(&sensorEventResource, data, w)
 }
 
 func (handler *HandlerService) IndexRoomSensorEventsByDate(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "id")
+	id := helpers.DecodeId(chi.URLParam(r, "id"))
+
 	date := chi.URLParam(r, "date")
 
 	start, err := helpers.ConvertStringToStartOfDay(date)
@@ -46,16 +47,15 @@ func (handler *HandlerService) IndexRoomSensorEventsByDate(w http.ResponseWriter
 		return
 	}
 
-	//roomInt, err := strconv.Atoi(id)
 	roomInt, err := helpers.TransformStringToInt(id)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	data, err := handler.use.GetAllDatasbyRoomByDate(roomInt, start, end)
+	data, _ := handler.use.GetAllDatasbyRoomByDate(roomInt, start, end)
 
-	var sensorEventResource []resources.SensorEventResource
+	var sensorEventResource []resources.SensorDataFromDate
 
 	resources.GenerateResource(&sensorEventResource, data, w)
 }
@@ -64,7 +64,7 @@ func (handler *HandlerService) IndexRoomSensorEventsBetweenTwoDates(w http.Respo
 	startDay := chi.URLParam(r, "date-debut")
 	endDay := chi.URLParam(r, "date-fin")
 
-	id := chi.URLParam(r, "id")
+	id := helpers.DecodeId(chi.URLParam(r, "id"))
 
 	start, err := helpers.ConvertStringToStartOfDay(startDay)
 	if err != nil {
@@ -87,14 +87,14 @@ func (handler *HandlerService) IndexRoomSensorEventsBetweenTwoDates(w http.Respo
 		return
 	}
 
-	var sensorEventResource []resources.SensorEventResource
+	var sensorEventResource []resources.SensorDataFromDate
 
 	resources.GenerateResource(&sensorEventResource, data, w)
 }
 
 func (handler *HandlerService) IndexSensorEvents(w http.ResponseWriter, r *http.Request) {
 	day := r.URL.Query().Get("day")
-	id := r.URL.Query().Get("id")
+	id := helpers.DecodeId(r.URL.Query().Get("id"))
 
 	idToInt, err := strconv.Atoi(id)
 	if err != nil {
@@ -126,8 +126,9 @@ func (handler *HandlerService) IndexSensorEvents(w http.ResponseWriter, r *http.
 
 func (handler *HandlerService) IndexSensorEventsByIdByRoomByDate(w http.ResponseWriter, r *http.Request) {
 
-	room := r.URL.Query().Get("room_id")
-	sensor := r.URL.Query().Get("sensor_id")
+	room := helpers.DecodeId(r.URL.Query().Get("room_id"))
+	sensor := helpers.DecodeId(r.URL.Query().Get("sensor_id"))
+
 	date := r.URL.Query().Get("date")
 
 	roomInt, err := helpers.TransformStringToInt(room)
