a474c1e9ab257fdba4b915dd43f4ff88db16009c
bipblop555 <tom.bonnet@hotmail.com>
2023-07-19 10:15:56 +0200

    pour front

--- app/main.go
+++ app/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"App/internal/handlers"
 	"App/internal/models"
 	"App/internal/route"
 	"fmt"
@@ -24,9 +23,9 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 
-	go handlers.SubscribeTopic(c)
+	//go handlers.SubscribeTopic(c)
 	//go handlers.SendRequest(c)
-	go handlers.StartSQL(c)
+	//go handlers.StartSQL(c)
 
 	if err != nil {
 		fmt.Println("Failed to connect to database:", err)
