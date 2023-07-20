f4f89d52287c981a6b3414cb8ccbea899778ea02
bipblop555 <tom.bonnet@hotmail.com>
2023-07-20 02:53:14 +0200

    websocket | form

--- app/main.go
+++ app/main.go
@@ -24,7 +24,7 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 
-	//go handlers.SubscribeTopic(c)
+	go handlers.SubscribeTopic(c)
 	//go handlers.SendRequest(c)
 	go handlers.StartSQL(c)
 
